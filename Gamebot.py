6abfc9231c5d4123fc07bf712cc5039fed11ab7c
theonekami <theonekamisama@gmail.com>
2018-05-13 16:25:39 +0530

    Update Gamebot.py

--- Gamebot.py
+++ Gamebot.py
@@ -13,10 +13,33 @@ import math ##for funsies
 
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="Game Boty WHEEE", command_prefix=("."), pm_help = False)
+client = commands.Bot(description="Game Boty WHEEE", command_prefix=("."), pm_help = False)
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
 # Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
+
+def rolld(args):
+        y=str(args).replace(" ","")
+        x=""
+        for i in y:
+                if i in ('+','-','*','/'): 
+                        break
+                x+=i
+        z=x.split('d')
+        no=int(z[0])
+        limit=int(z[1])
+        rolls=list()
+        for i in range(no):
+                rolls.append(random.randint(1,limit))
+        res="Roll(s):"
+        for i in rolls:
+                res+=" "+str(i)
+        res+=" || Sum="
+        s= str(sum(rolls))
+        y=y.replace(x,s)
+        res+=str(eval(y))
+        return res
+
 @client.event
 async def on_ready():
         print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
@@ -43,27 +66,9 @@ async def hi(ctx):
         await ctx.send("Hello I am game bot...Plz be my fren")
 
 @client.command()
-async def roll(ctx,*,args):
+async def rolld(ctx,*,args):
         """Rolls a dice. Formatted as  <no of dice>d<no of sides> eg. 3d10"""
-        y=str(args).replace(" ","")
-        x=""
-        for i in y:
-                if i in ('+','-','*','/'): 
-                        break
-                x+=i
-        z=x.split('d')
-        no=int(z[0])
-        limit=int(z[1])
-        rolls=list()
-        for i in range(no):
-                rolls.append(random.randint(1,limit))
-        res="Roll(s):"
-        for i in rolls:
-                res+=" "+str(i)
-        res+=" || Sum="
-        s= str(sum(rolls))
-        y=y.replace(x,s)
-        res+=str(eval(y))
+        res=roll(args)
         await ctx.send(res)
 
 @client.command()
@@ -73,4 +78,4 @@ async def pick(*, args):
         x=random.choice(y.split(','))
         await ctx.send("I give you: "+x)
 
-client.run('NDE1MTQ5Mzk5NjUyNTY0OTk0.DW27KQ.vvG08o51-BQZBYc8TFPDJbm0RLM')
+client.run('NDE1MTQ5Mzk5NjUyNTY0OTk0.DW27KQ.vvG08o51-BQZBYc8TFPDJbm0RLM')  ​
