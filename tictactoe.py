b37714d192cf01c7af1c48a769d1d5e8e79fc116
theonekami <theonekamisama@gmail.com>
2018-05-14 23:10:03 +0530

    k fixed draw

--- tictactoe.py
+++ tictactoe.py
@@ -4,15 +4,11 @@ from discord.ext import commands
 class TicTacToe:
     def __init__(self, bot):
         self.bot=bot
-
-    @commands.command()
-    async def draw(self,ctx):
-
-        x="""
+        board_img="""
 
 
 
-:arrow_upper_left::one: :two: :three: 
+:arrow_upper_left::one::two::three: 
 :one::large_blue_circle::large_blue_circle::large_blue_circle:
 :two::large_blue_circle::large_blue_circle::large_blue_circle:
 :three::large_blue_circle::large_blue_circle::large_blue_circle:
@@ -20,7 +16,11 @@ class TicTacToe:
 
 
 """
-        em= discord.Embed(title="Tic Tac Toe.",description=x)
+        board_array=[[0,0,0],[0,0,0],[0,0,0]]
+
+    @commands.command()
+    async def draw(self,ctx):
+        em= discord.Embed(title="Tic Tac Toe.",description=self.board_img)
         await ctx.send(embed=em)
 ##        await ctx.send(x)
 
