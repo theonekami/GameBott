80f478aafb5cb4d036a00615a33330510486362d
theonekami <theonekamisama@gmail.com>
2018-05-14 23:35:12 +0530

    Somehow organized

--- tictactoe.py
+++ tictactoe.py
@@ -4,26 +4,28 @@ from discord.ext import commands
 class TicTacToe:
     def __init__(self, bot):
         self.bot=bot
-        self.board_img="""
-
-
-
-:arrow_upper_left::one::two::three: 
-:one::red_circle::red_circle::red_circle:
-:two::red_circle::red_circle::red_circle:
-:three::red_circle::red_circle::red_circle:
-
-
-
-"""
         self.blank="red_circle"
         self.cross="crossed_swords"
         self.circle="shield"
+        self.board_img=[
+[":arrow_upper_left::one::two::three:\n"],
+[":one:",self.blank,self.blank,self.blank,"\n"],
+[":two:",self.blank,self.blank,self.blank,"\n"],
+[":three:",self.blank,self.blank,self.blank,"\n"]
+]
+
         self.board_array=[[0,0,0],[0,0,0],[0,0,0]]
+
+    def board(self):
+        x=""
+        for i in board_img:
+            for j in i:
+                x+=j
+        return x
 
     @commands.command()
     async def draw(self,ctx):
-        em= discord.Embed(description=self.board_img)
+        em= discord.Embed(description=board())
         await ctx.send(embed=em)
 ##        await ctx.send(x)
 
