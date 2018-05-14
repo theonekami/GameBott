393665ea7b05a5ea1ca78478396527aea2c70d82
theonekami <theonekamisama@gmail.com>
2018-05-14 22:45:30 +0530

    Genius 2

--- tictactoe.py
+++ tictactoe.py
@@ -9,14 +9,9 @@ class TicTacToe:
     async def draw(self,ctx):
 
         x="""
-```
-
-  ¦ 1  ¦  2  ¦  3  ¦
-  ----------------
-  ¦ 4  ¦  5  ¦  6  ¦
-  ----------------
-  ¦ 7  ¦  8  ¦  9  ¦
-```
+:large_blue_circle::large_blue_circle::large_blue_circle:
+:large_blue_circle::large_blue_circle::large_blue_circle:
+:large_blue_circle::large_blue_circle::large_blue_circle:
 """
         discord.Embed(title="Tic Tac Toe.",discription=x)
         await ctx.send(embed=em)
