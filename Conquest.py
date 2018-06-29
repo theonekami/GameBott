cc942072be8b6323fe3991b7dab5145f3dc210f9
Kami <theonekamisama@gmail.com>
2018-06-29 21:39:51 +0530

    GET REKT

--- Conquest.py
+++ Conquest.py
@@ -13,26 +13,26 @@ class Conquest:
     async def cq_ice(self, ctx):
         em = discord.Embed()
         Name= "Glass Evans"
-        quote= "Might is Right"
+        quote= '"*Might is Right*"'
         ace= "Kyurem-Black"
         img="https://pre00.deviantart.net/02b7/th/pre/i/2012/064/6/4/black_kyurem_by_namh-d4rrv3d.jpg"
         em.set_image(url=img)
-        em.add_field(name="Name", value=Name,inline=False)
-        em.add_field(name="Quote", value=quote,inline=False)
-        em.add_field(name="Ace", value=ace,inline=False)
+        em.add_field(name="**Name**", value=Name,inline=False)
+        em.add_field(name="**Quote**", value=quote,inline=False)
+        em.add_field(name="**Ace**", value=ace,inline=False)
         await ctx.send(embed=em)
 
     @cq.command(name= "fire")
     async def cq_fire(self, ctx):
         em = discord.Embed()
-        Name= "**Razan Atmadh**"
-        quote= "**The Dead See All**"
+        Name= "Razan Atmadh**"
+        quote= "*The Dead See All*"
         ace= "Mega-Houndoom"
-        img="https://cdn.discordapp.com/attachments/456079885769768961/462283728241164288/mega_houndoom_by_theblacksavior-d7ivyjl.png"
+        img="https://orig00.deviantart.net/7bf1/f/2014/138/7/b/mega_houndoom_by_theblacksavior-d7ivyjl.jpg"
         em.set_image(url=img)
-        em.add_field(name="Name", value=Name,inline=False)
-        em.add_field(name="Quote", value=quote,inline=False)
-        em.add_field(name="Ace", value=ace,inline=False)
+        em.add_field(name="**Name**", value=Name,inline=False)
+        em.add_field(name="**Quote**", value=quote,inline=False)
+        em.add_field(name="**Ace**", value=ace,inline=False)
         await ctx.send(embed=em)
 
 ##    @cq.command(name= "Flying")
