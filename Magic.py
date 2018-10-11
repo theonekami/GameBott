609b794d997174fff1785f1eaa60d6be9bbf1e48
Kami <theonekamisama@gmail.com>
2018-10-11 20:20:19 +0530

    go

--- Magic.py
+++ Magic.py
@@ -23,27 +23,27 @@ class Magic:
             await ctx.send("Card name " + str(args) + " not found")
 
 
-class HS:
-    def __init__(self, bot):
-        self.bot=bot
-
-    @commands.command()
-    async def hscard(self,ctx,args):
-        x=str(args)
-        x=x.replace(" ","%20")
-        y=None
-        h={"X-Mashape-Key": "nZsSdxxcN1mshskGiEd18AIpXDksp19XabQjsn8LotoIfnfv54","Accept": "application/json"}
-        async with aiohttp.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/"+x,headers=h)) as res:
-            print(res.status)
-            y=json.loads(await res.text())
-        res.close()
-        try:
-            em = discord.Embed(title=y[[0]['name'])
-            em.set_image(url=y[0]['img'])
-            await ctx.send(embed= em)
-        except:
-            await ctx.send("Card name " + str(args) + " not found")
+##class HS:
+##    def __init__(self, bot):
+##        self.bot=bot
+##
+##    @commands.command()
+##    async def hscard(self,ctx,args):
+##        x=str(args)
+##        x=x.replace(" ","%20")
+##        y=None
+##        h={"X-Mashape-Key": "nZsSdxxcN1mshskGiEd18AIpXDksp19XabQjsn8LotoIfnfv54","Accept": "application/json"}
+##        async with aiohttp.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/"+x,headers=h)) as res:
+##            print(res.status)
+##            y=json.loads(await res.text())
+##        res.close()
+##        try:
+##            em = discord.Embed(title=y[[0]['name'])
+##            em.set_image(url=y[0]['img'])
+##            await ctx.send(embed= em)
+##        except:
+##            await ctx.send("Card name " + str(args) + " not found")
 
 def setup(bot):
     bot.add_cog(Magic(bot))
-    bot.add_cog(HS(bot))
+   # bot.add_cog(HS(bot))
