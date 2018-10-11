7facf4bdcaf724528754794bddfc09f29b852645
Kami <theonekamisama@gmail.com>
2018-10-11 19:27:55 +0530

    go

--- Magic.py
+++ Magic.py
@@ -11,12 +11,13 @@ class Magic:
     async def card(self,ctx,args):
         y=str(args)
         async with aiohttp.get('https://api.magicthegathering.io/v1/cards?name="'+y+'"') as res:
+            print(res.status)
             if (res.status==200):
                 y=json.loads(await res.text())
             else:
                 await ctx.send("Card named" + str(args)+" not found")
                 res.close()
-                return
+                return 0
         res.close()
         em = discord.Embed(title=y['cards'][0]['name'])
         em.set_image(url=y['cards'][0]['imageUrl'])
