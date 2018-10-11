b92f1f526cbd631b43475ad3a6f83ec1deb47b6e
Kami <theonekamisama@gmail.com>
2018-10-11 19:14:00 +0530

    go

---
+++ Magic.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import json
+import aiohttp
+
+class Magic:
+    def __init__(self, bot):
+        self.bot=bot
+
+    @commands.command()
+    async def card(self,ctx,args):
+        y=str(args)
+        async with aiohttp.get('https://api.magicthegathering.io/v1/cards?name="'+y+'"') as res:
+            if (res.status_code==200):
+                y=json.loads(res.text)
+            else:
+                await ctx.send("Card Not Found")
+                res.close()
+                return
+        res.close()
+        em = discord.Embed(title=y['cards'][0]['name'])
+        em.set_image(url=y['cards'][0]['imageUrl'])
+        await ctx.send(embed= em) 
+
+def setup(bot):
+    bot.add_cog(Magic(bot))
