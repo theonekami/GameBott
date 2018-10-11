748fbb6f5e0c7428790faf9b7990e6864eea00d1
Kami <theonekamisama@gmail.com>
2018-10-11 19:32:50 +0530

    dear god help me

--- Magic.py
+++ Magic.py
@@ -9,19 +9,18 @@ class Magic:
 
     @commands.command()
     async def card(self,ctx,args):
-        y=str(args)
-        async with aiohttp.get('https://api.magicthegathering.io/v1/cards?name="'+y+'"') as res:
+        x=str(args)
+        y=None
+        async with aiohttp.get('https://api.magicthegathering.io/v1/cards?name="'+x+'"') as res:
             print(res.status)
-            if (res.status==200):
-                y=json.loads(await res.text())
-            else:
-                await ctx.send("Card named" + str(args)+" not found")
-                res.close()
-                return 0
+            y=json.loads(await res.text())
         res.close()
-        em = discord.Embed(title=y['cards'][0]['name'])
-        em.set_image(url=y['cards'][0]['imageUrl'])
-        await ctx.send(embed= em) 
+        if(not len(y)):
+            em = discord.Embed(title=y['cards'][0]['name'])
+            em.set_image(url=y['cards'][0]['imageUrl'])
+            await ctx.send(embed= em)
+        else:
+            await ctx.send("Card name " + str(args) + " not found")
 
 def setup(bot):
     bot.add_cog(Magic(bot))
